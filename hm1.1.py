5beb323c684b37adda2e7605482e3b8072d3edfd
YumiYukiko <eka112412@gmail.com>
2019-02-28 21:24:40 +0300

    a>=0, b>=0 instead of a>0 and b>0

--- hm1.1.py
+++ hm1.1.py
@@ -1,15 +1,15 @@
 def NOD (a,b):
-    while a !=0 and b !=0 :
+    while a != 0 and b != 0 :
         if a>b:
             a = a % b
         else:
             b = b % a
     return a+b
     
-a = 0
-b = 0
+a = -1
+b = -1
 
-while a<=0 or b<=0:
+while a<0 or b<0:
     a = float(input("a = "))
     b = float(input("b = "))
 
